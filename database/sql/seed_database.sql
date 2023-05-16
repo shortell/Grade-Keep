92c68803c0cb48a3ec5fe5d00811acd0a5bbf7cd
Jack-S-RIT <jms5051@rit.edu>
2023-05-16 18:12:08 -0400

    added seed file

---
+++ database/sql/seed_database.sql
@@ -0,0 +1,7 @@
+-- teachers
+INSERT INTO teachers (first_name, last_name, username, password)
+VALUES ('Sidney', 'Velazquez', 'sv123', 'password'); -- id: 1
+INSERT INTO teachers (first_name, last_name, username, password)
+VALUES ('Frances', 'Mccann', 'fm456', 'apples'); -- id: 2
+INSERT INTO teachers (first_name, last_name, username, password)
+VALUES ('Neo', 'Brewer', 'nb789', 'apples'); -- id: 3
