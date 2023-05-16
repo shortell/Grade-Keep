e896d109cc3875e456f8ee077be8abd9bd367eba
Jack-S-RIT <jms5051@rit.edu>
2023-05-16 18:13:44 -0400

    added and tested create teacher function

--- database/teacher.py
+++ database/teacher.py
@@ -1,9 +1,22 @@
 from database.postgres_utils import exec_commit
 from database.db_utils import hash_password
+import psycopg2
 
-def register(username, password):
+def create_teacher(username, password):
+    """
+    attempts to add record to teacher table if username is unique
+    unique id generated, first and last name set to empty strings
+    
+    :param username: a string
+    :param password: a string that gets hashed
+    :returns: True if teacher is created and False otherwise
+    """
     query = """
     INSERT INTO teachers (username, password)
     VALUES (%s, %s);
     """
-    return exec_commit(query, (username, hash_password(password)))
+    try:
+        exec_commit(query, (username, hash_password(password)))
+        return True
+    except psycopg2.errors.UniqueViolation:
+        return False
--- tests/test_teacher.py
+++ tests/test_teacher.py
@@ -1,5 +1,5 @@
 import unittest
-from database.db_utils import initialize_database
+from database.db_utils import initialize_database, seed_database
 from database.postgres_utils import exec_get_all
 from database.teacher import *
 
@@ -8,12 +8,15 @@ class Test_Teacher(unittest.TestCase):
 
     def setUp(self):
         initialize_database()
+        seed_database()
+        
 
-    def test_register(self):
-        register("johnSmith123", "password")
-        query = """
-        SELECT * FROM teachers;
-        """
-        expected = [(1, '', '', 'johnSmith123', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'), ]
-        actual = exec_get_all(query)
+    def test_create_teacher_successfully(self):
+        actual = create_teacher("johnSmith123", "password")
+        expected = True
+        self.assertEqual(actual, expected)
+
+    def test_create_teacher_failed(self):
+        actual = create_teacher("sv123", "password") 
+        expected = False
         self.assertEqual(actual, expected)
