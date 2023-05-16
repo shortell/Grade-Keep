0f7a573e9ecdd02909ef31ae1ab782d5a931ae5a
Jack-S-RIT <jms5051@rit.edu>
2023-05-16 18:42:28 -0400

    tweaked seed data

--- database/sql/seed_database.sql
+++ database/sql/seed_database.sql
@@ -1,7 +1,7 @@
 -- teachers
 INSERT INTO teachers (first_name, last_name, username, password)
-VALUES ('Sidney', 'Velazquez', 'sv123', 'password'); -- id: 1
+VALUES ('Sidney', 'Velazquez', 'sv123', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'); -- id: 1, password: password
 INSERT INTO teachers (first_name, last_name, username, password)
-VALUES ('Frances', 'Mccann', 'fm456', 'apples'); -- id: 2
+VALUES ('Frances', 'Mccann', 'fm456', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'); -- id: 2, password: password
 INSERT INTO teachers (first_name, last_name, username, password)
-VALUES ('Neo', 'Brewer', 'nb789', 'apples'); -- id: 3
+VALUES ('Neo', 'Brewer', 'nb789', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'); -- id: 3, password: password
