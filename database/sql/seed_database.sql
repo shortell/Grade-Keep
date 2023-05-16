a9b7a7fe9a4715dce38cadbfab63742c1c233910
Jack-S-RIT <jms5051@rit.edu>
2023-05-16 19:11:44 -0400

    updated seed data

--- database/sql/seed_database.sql
+++ database/sql/seed_database.sql
@@ -1,7 +1,23 @@
 -- teachers
-INSERT INTO teachers (first_name, last_name, username, password)
-VALUES ('Sidney', 'Velazquez', 'sv123', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'); -- id: 1, password: password
-INSERT INTO teachers (first_name, last_name, username, password)
-VALUES ('Frances', 'Mccann', 'fm456', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'); -- id: 2, password: password
-INSERT INTO teachers (first_name, last_name, username, password)
-VALUES ('Neo', 'Brewer', 'nb789', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'); -- id: 3, password: password
+INSERT INTO teachers (first_name, last_name, username, password) -- id: 1, password: password
+VALUES ('Sidney', 'Velazquez', 'sv123', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'); 
+INSERT INTO teachers (first_name, last_name, username, password) -- id: 2, password: password
+VALUES ('Frances', 'Mccann', 'fm456', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'); 
+INSERT INTO teachers (first_name, last_name, username, password) -- id: 3, password: password
+VALUES ('Neo', 'Brewer', 'nb789', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'); 
+
+-- classes
+INSERT INTO classes (title, teacher_id) -- id: 1
+VALUES ('Algebra I section 01', 1);
+
+INSERT INTO classes (title, teacher_id) -- id: 2
+VALUES ('Algebra I section 02', 1);
+
+INSERT INTO classes (title, teacher_id) -- id: 3
+VALUES ('AP U.S. History', 2);
+
+INSERT INTO classes (title, teacher_id) -- id: 4
+VALUES ('AP US History', 3);
+
+INSERT INTO classes (title, teacher_id) -- id: 5
+VALUES ('Global Studies', 3);
