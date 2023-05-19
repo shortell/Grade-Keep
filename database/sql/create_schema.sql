1c5cd09b3a0aa92d2fbd9758e585df0b91c8ab5f
Jack-S-RIT <jms5051@rit.edu>
2023-05-19 17:28:14 -0400

    updated schema

--- database/sql/create_schema.sql
+++ database/sql/create_schema.sql
@@ -24,29 +24,37 @@ CREATE TABLE classes (
 
 CREATE TABLE enrollments (
     id SERIAL NOT NULL PRIMARY KEY,
-    student_id integer NOT NULL,
-    class_id integer NOT NULL
+    student_id integer NOT NULL references students(id),
+    class_id integer NOT NULL references classes(id)
 );
 
 CREATE TABLE assignments (
     id SERIAL NOT NULL,
     title text NOT NULL,
     instructions text NOT NULL,
-    class_id integer NOT NULL
+    due_date_time TIMESTAMP NOT NULL,
+    class_id integer NOT NULL references classes(id)
     
 );
 
 CREATE TABLE submissions (
     id SERIAL NOT NULL,
     response text,
-    assignment_id integer NOT NULL,
-    student_id integer NOT NULL
+    assignment_id integer NOT NULL references assignments(id),
+    student_id integer NOT NULL references students(id),
+    submitted_date_time TIMESTAMP NOT NULL
 );
 
 CREATE TABLE grades (
     id SERIAL NOT NULL PRIMARY KEY,
     title text NOT NULL,
-    points_earned decimal DEFAULT NULL,
     total_points decimal NOT NULL,
-    enrollment_id integer NOT NULL references enrollments(id)
+    class_id integer NOT NULL references classes(id)
 );
+
+CREATE TABLE scores (
+    id SERIAL NOT NULL PRIMARY KEY,
+    points_earned decimal,
+    grade_id integer NOT NULL references grades(id),
+    student_id integer NOT NULL references students(id)
+)
