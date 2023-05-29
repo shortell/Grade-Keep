472df1c943646368e6b239cc3492441593927b5d
Jack-S-RIT <jms5051@rit.edu>
2023-05-29 11:20:45 -0400

    changed import for teacher api to allow for student api to have same resource names

--- server/server.py
+++ server/server.py
@@ -2,9 +2,10 @@ from flask_cors import CORS
 from flask_restful import Api
 from flask import Flask
 from rest_api.auth_api import Login, Logout, Register
-from rest_api.teacher_api import (Teachers, Courses, Course, Assignments,
-                                  Assignment, Students, Student, Submissions, Submission, Grades, Grade, Scores, Score)
+# from rest_api.teacher_api import (Teachers, Courses, Course, Assignments,
+#                                   Assignment, Students, Student, Submissions, Submission, Grades, Grade, Scores, Score)
 from db import db_utils
+from rest_api import teacher_api
 
 
 app = Flask(__name__)  # create Flask instance
@@ -18,19 +19,19 @@ grade_url = "/grades/<int:grade_id>"
 api.add_resource(Login, "/login")
 api.add_resource(Logout, "/logout")
 api.add_resource(Register, "/register")
-api.add_resource(Teachers, "/teachers")
-api.add_resource(Courses, "/teachers/courses")
-api.add_resource(Course, course_url)
-api.add_resource(Students, course_url + "/students")
-api.add_resource(Student, course_url + "/students/<int:enrollment_id>")
-api.add_resource(Assignments, course_url + "/assignments")
-api.add_resource(Assignment, course_url + assignment_url)
-api.add_resource(Submissions, course_url + assignment_url + "/submissions")
-api.add_resource(Submission, course_url + assignment_url + "/submissions/<int:submission_id>")
-api.add_resource(Grades, course_url + "/grades")
-api.add_resource(Grade, course_url + grade_url)
-api.add_resource(Scores, course_url + grade_url + "/scores")
-api.add_resource(Score, course_url + grade_url + "/scores/<int:score_id>")
+api.add_resource(teacher_api.Teachers, "/teachers")
+api.add_resource(teacher_api.Courses, "/teachers/courses")
+api.add_resource(teacher_api.Course, course_url)
+api.add_resource(teacher_api.Students, course_url + "/students")
+api.add_resource(teacher_api.Student, course_url + "/students/<int:enrollment_id>")
+api.add_resource(teacher_api.Assignments, course_url + "/assignments")
+api.add_resource(teacher_api.Assignment, course_url + assignment_url)
+api.add_resource(teacher_api.Submissions, course_url + assignment_url + "/submissions")
+api.add_resource(teacher_api.Submission, course_url + assignment_url + "/submissions/<int:submission_id>")
+api.add_resource(teacher_api.Grades, course_url + "/grades")
+api.add_resource(teacher_api.Grade, course_url + grade_url)
+api.add_resource(teacher_api.Scores, course_url + grade_url + "/scores")
+api.add_resource(teacher_api.Score, course_url + grade_url + "/scores/<int:score_id>")
 
 
 if __name__ == '__main__':
